72ac84a7eeddecd9aaedac6b2135257c3737b11d
Rens Oliemans <hallo@rensoliemans.nl>
2021-10-31 14:55:17 +0100

    :wrench: Clean up config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/ptm-reflection/' : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ptm-reflection/'
+    : '/'
 }
